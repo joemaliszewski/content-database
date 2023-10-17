1ad1541ba70e4a8f693f8ec2e1b2d98bba05f39f
Joe Maliszewski <joseph.maliszewski93@gmail.com>
2023-10-17 20:58:14 +0100

    update infra

--- infra/security.tf
+++ infra/security.tf
@@ -1,18 +1,11 @@
-# resource "aws_db_subnet_group" "rds_subnet_group" {
-#   name       = "my-rds-subnet-group"
-#   subnet_ids = var.subnet_ids
+# Define a security group that allows access only from a list of IP addresses.
+resource "aws_security_group" "rds_sg" {
+  vpc_id = data.aws_vpc.vpc.id
 
-#   tags = {
-#     Name = "My RDS Subnet Group"
-#   }
-# }
-
-# resource "aws_security_group" "rds_sg" {
-#   vpc_id = data.aws_vpc.vpc.id
-#   ingress {
-#     from_port   = 3306
-#     to_port     = 3306
-#     protocol    = "tcp"
-#     cidr_blocks = var.trusted_ips
-#   }
-# }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = var.trusted_ips
+  }
+}
